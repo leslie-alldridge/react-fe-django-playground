a5ab2827534338761b52f99d438f59d4fb534373
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-22 21:54:19 +1300

    form signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password1: "",
+      password2: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios
+      .post("http://localhost:8000/accounts/signup/", this.state)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3>This is going to be a register form hopefully</h3>
+          <form onSubmit={this.handleSubmit}>
+            {/* {% csrf_token %} */}
+            {/* {{ form.as_p }} */}
+            <input onChange={this.handleChange} type="email" name="email" />
+            <input
+              onChange={this.handleChange}
+              type="password"
+              name="password1"
+            />
+            <input
+              onChange={this.handleChange}
+              type="password"
+              name="password2"
+            />
+
+            <button type="submit">Sign up</button>
+          </form>
         </header>
       </div>
     );
